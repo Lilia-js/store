df00e81e50d9cae23eb161e2f509bad9a02c6df9
unknown <liliya.shtepenko@gmail.com>
2021-07-06 11:16:06 +0300

    test

--- src/app/admin/shared/admin-layout/admin-layout.component.ts
+++ src/app/admin/shared/admin-layout/admin-layout.component.ts
@@ -1,27 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/shared/auth.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "src/app/shared/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-admin-layout',
-  templateUrl: './admin-layout.component.html',
-  styleUrls: ['./admin-layout.component.scss']
+  selector: "app-admin-layout",
+  templateUrl: "./admin-layout.component.html",
+  styleUrls: ["./admin-layout.component.scss"],
 })
 export class AdminLayoutComponent implements OnInit {
+  constructor(public auth: AuthService, private router: Router) {}
 
-  constructor(
-    public auth: AuthService,
-    private router: Router
-  ) { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   logout($event) {
-    event.preventDefault()
-    this.auth.logout()
-    this.router.navigate(['/admin','login'])
-
+    event.preventDefault();
+    this.auth.logout();
+    this.router.navigate(["/admin", "login"]);
   }
-
 }
